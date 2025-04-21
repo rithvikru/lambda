79f64fd728d900257538246c6195f3f294555d05
Rithvik Redrouthu <rithvik.redrouthu@gmail.com>
2025-04-20 22:22:12 -0400

    toy parsers

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,4 +1,30 @@
 module MyLib (someFunc) where
 
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Data.Void
+
+type Parser = Parsec Void String
+
+parse_a :: Parser Char
+parse_a = char 'a'
+
+parse_b :: Parser Char
+parse_b = char 'b'
+
+parse_ab :: Parser String
+parse_ab = do
+  a <- parse_a
+  b <- parse_b
+  return [a, b]
+
+parse_cat :: Parser String
+parse_cat = do
+  a <- parse_a
+  b <- symbolChar
+  c <- upperChar
+  d <- digitChar
+  return [a, b, c, d]
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
